c0eb6ef9db85f8b87c58bf44c2b5df0e0d1bf9c9
DaoTienDat5345 <166401580+DaoTienDat5345@users.noreply.github.com>
2025-10-23 00:54:30 +0700

    Introduce

---
+++ Quiz_Web/Controllers/IntroduceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Quiz_Web.Controllers
+{
+    public class IntroduceController : Controller
+    {
+        private readonly ILogger<IntroduceController> _logger;
+
+        public IntroduceController(ILogger<IntroduceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/admin/Introduce")]
+        public IActionResult Index()
+        {
+            return View("~/Views/Introduce.cshtml");
+        }
+    }
+}
